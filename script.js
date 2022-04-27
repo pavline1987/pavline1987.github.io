28165d2216bb9ed0bfd867d3c5b1608cff6c7515
pavline1987 <seb.dominique87@pm.me>
2022-04-27 18:54:04 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // api url
 const api_url =
-	"https://employeedetails.free.beeceptor.com/my/api/path";
+	"https://api.cronoscan.com/api?module=account&action=balance&address=0xCeb02a59807Cb272ce76a843857877BaB2821352&apikey=G5M7R3YXI95C3J6UVI443X61B2RMHFUB4K";
 
 // Defining async function
 async function getapi(url) {
@@ -27,19 +27,17 @@ function hideloader() {
 function show(data) {
 	let tab =
 		`<tr>
-		<th>Name</th>
-		<th>Office</th>
-		<th>Position</th>
-		<th>Salary</th>
+		<th>status</th>
+		<th>message</th>
+		<th>result</th>
 		</tr>`;
 	
 	// Loop to access all rows
 	for (let r of data.list) {
 		tab += `<tr>
-	<td>${r.name} </td>
-	<td>${r.office}</td>
-	<td>${r.position}</td>
-	<td>${r.salary}</td>		
+	<td>${status} </td>
+	<td>${message}</td>
+	<td>${result}</td>		
 </tr>`;
 	}
 	// Setting innerHTML as tab variable
