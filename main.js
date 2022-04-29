d38d1601b827dce65455af2705fd6ea1523e0a36
pavline1987 <seb.dominique87@pm.me>
2022-04-28 21:11:36 -0400

    s

---
+++ main.js
@@ -0,0 +1,22 @@
+$(document).ready(() => {
+  const $showData = $('#show-data');
+  const $raw = $('pre');
+
+  $('#get-data').on('click', (e) => {
+    e.preventDefault();
+
+    $showData.text('Loading the JSON file.');
+
+    $.getJSON('example.json', (data) => {
+      const markup = data.items
+        .map(item => `<li>${item.key}: ${item.value}</li>`)
+        .join('');
+
+      const list = $('<ul />').html(markup);
+
+      $showData.html(list);
+
+      $raw.text(JSON.stringify(data, undefined, 2));
+    });
+  });
+});
