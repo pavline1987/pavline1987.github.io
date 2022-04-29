818adf6bc770906021342aee233d98b7ac61a2d4
pavline1987 <seb.dominique87@pm.me>
2022-04-28 22:16:08 -0400

    Update main.js

--- src/main.js
+++ src/main.js
@@ -9,7 +9,7 @@ $(document).ready(() => {
 
     $.getJSON('api.json', (data) => {
       const markup = data.result
-        .map(item => `<li>${result.blockNumber}: ${result.timeStamp}</li>`)
+        .map(item => `<li>${item.blockNumber}: ${item.timeStamp}</li>`)
         .join('');
 
       const list = $('<ul />').html(markup);
