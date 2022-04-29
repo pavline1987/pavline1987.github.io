500b91743973ff4aceae6af6077599f988abd7c5
pavline1987 <seb.dominique87@pm.me>
2022-04-28 22:23:51 -0400

    2

---
+++ src/main2.js
@@ -0,0 +1,22 @@
+$(document).ready(() => {
+  const $showData = $('#show-data');
+  const $raw = $('pre');
+
+  $('#get-data').on('click', (e) => {
+    e.preventDefault();
+
+    $showData.text('Loading the JSON file.');
+
+    $.getJSON('api.json', (data) => {
+      const markup = data.result
+        .map(item => `<li>${item.blockNumber}: ${item.timeStamp}</li>`)
+        .join('');
+
+      const list = $('<ul />').html(markup);
+
+      $showData.html(list);
+
+      $raw.text(JSON.stringify(data, undefined, 2));
+    });
+  });
+});
