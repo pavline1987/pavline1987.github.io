062273887dca19d4511ee3035822c9622f6a6bb2
pavline1987 <seb.dominique87@pm.me>
2022-04-28 22:01:24 -0400

    sa

--- src/main.js
+++ src/main.js
@@ -8,8 +8,8 @@ $(document).ready(() => {
     $showData.text('Loading the JSON file.');
 
     $.getJSON('https://api.cronoscan.com/api?module=account&action=txlist&address=0x9631945CF72b9C7c6B3f0790A4a79e82941beea9&startblock=1&endblock=99999999&sort=asc&apikey=G5M7R3YXI95C3J6UVI443X61B2RMHFUB4K', (data) => {
-      const markup = data.items
-        .map(item => `<li>${result.blockNumber}: ${result.timeStamp}</li>`)
+      const markup = data.result
+        .map(item => `<li>${blockNumber}</li>`)
         .join('');
 
       const list = $('<ul />').html(markup);
