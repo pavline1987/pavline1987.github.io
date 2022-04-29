2523652f219b2677ab7b6bb62a584c72c5fe2def
pavline1987 <seb.dominique87@pm.me>
2022-04-28 22:05:33 -0400

    Update main.js

--- src/main.js
+++ src/main.js
@@ -7,7 +7,7 @@ $(document).ready(() => {
 
     $showData.text('Loading the JSON file.');
 
-    $.getJSON('https://api.cronoscan.com/api?module=account&action=txlist&address=0x9631945CF72b9C7c6B3f0790A4a79e82941beea9&startblock=1&endblock=99999999&sort=asc&apikey=G5M7R3YXI95C3J6UVI443X61B2RMHFUB4K', (data) => {
+    $.getJSON('api.json', (data) => {
       const markup = data.result
         .map(item => `<li>${blockNumber}</li>`)
         .join('');
